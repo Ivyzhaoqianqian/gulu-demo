bc5c2a1ada00cfc7d5d84387856b02e0001ab2a8
Ivyzhao <1490217104@qq.com>
2022-05-08 16:11:13 +0800

    使用chai.expect添加四个测试用力

--- src/app.js
+++ src/app.js
@@ -16,3 +16,89 @@ new Vue({
         loading3: false
     }
 })
+
+
+
+import chai from 'chai'
+const expect = chai.expect
+// 单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#isettings')
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#iloading')
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq("1")
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq("2")
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const gButton = new Constructor({
+        propsData: {
+            icon: 'settings',
+
+        }
+    })
+    gButton.$mount()
+    gButton.$on('click', function () {
+
+    })
+    let button = gButton.$el
+    button.click()
+
+}
