028b0e83ae714335b45c7258c2bee8d5e2f93d28
Ivyzhao <1490217104@qq.com>
2022-05-16 16:00:42 +0800

    解决当容器有overflow hidden造成的bug

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -14215,6 +14215,8 @@ exports.default = void 0;
 //
 //
 //
+//
+//
 var _default = {
   name: "GuluPopover",
   data: function data() {
@@ -14222,6 +14224,7 @@ var _default = {
       visible: false
     };
   },
+  mounted: function mounted() {},
   methods: {
     xxx: function xxx() {
       var _this = this;
@@ -14230,6 +14233,17 @@ var _default = {
 
       if (this.visible === true) {
         this.$nextTick(function () {
+          document.body.appendChild(_this.$refs.contentWrapper);
+
+          var _this$$refs$triggerWr = _this.$refs.triggerWrapper.getBoundingClientRect(),
+              width = _this$$refs$triggerWr.width,
+              height = _this$$refs$triggerWr.height,
+              top = _this$$refs$triggerWr.top,
+              left = _this$$refs$triggerWr.left;
+
+          _this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+          _this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+
           var eventHandler = function eventHandler() {
             _this.visible = false;
             document.removeEventListener('click', eventHandler);
@@ -14269,22 +14283,14 @@ exports.default = _default;
       _vm.visible
         ? _c(
             "div",
-            {
-              staticClass: "content-wrapper",
-              on: {
-                click: function ($event) {
-                  $event.stopPropagation()
-                },
-              },
-            },
+            { ref: "contentWrapper", staticClass: "content-wrapper" },
             [_vm._t("content")],
             2
           )
         : _vm._e(),
       _vm._v(" "),
-      _vm._t("default"),
-    ],
-    2
+      _c("span", { ref: "triggerWrapper" }, [_vm._t("default")], 2),
+    ]
   )
 }
 var staticRenderFns = []
--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop> 
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" > 
         <slot name="content" ></slot>
     </div>
+    <span ref="triggerWrapper">
         <slot ></slot>
+    </span>
     </div>
 </template>
 
@@ -13,11 +15,20 @@
         data(){
             return {visible:false}
         },
+        mounted(){
+            
+        },
         methods:{
             xxx(){
                 this.visible = !this.visible
                 if(this.visible === true){
+                    
                     this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left +window.scrollX+ 'px'
+                        this.$refs.contentWrapper.style.top = top +window.scrollY+ 'px'
+
                         let eventHandler= ()=>{
                             this.visible = false
                             document.removeEventListener('click',eventHandler)
@@ -29,7 +40,8 @@
 
                 }
             }
-        }
+        },
+        
     }
 </script>
 
@@ -38,12 +50,12 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper{
+    }
+    .content-wrapper{
             position: absolute;
-            bottom:100%;
-            left: 0;
             border:1px solid red;
-            box-shadow:0 0 3px rgba( 0,0, 0,0.5) ;
-        }
+            box-shadow:0 0 3px rgba( 0,0, 0,0.5);
+            transform: translateY(-100%);
+             
     }
 </style>
