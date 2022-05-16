32094111939ede70e07dd0f403452fa460b41c48
Ivyzhao <1490217104@qq.com>
2022-05-16 15:33:23 +0800

    实现最简单的popover

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -14224,7 +14224,20 @@ var _default = {
   },
   methods: {
     xxx: function xxx() {
+      var _this = this;
+
       this.visible = !this.visible;
+
+      if (this.visible === true) {
+        this.$nextTick(function () {
+          var eventHandler = function eventHandler() {
+            _this.visible = false;
+            document.removeEventListener('click', eventHandler);
+          };
+
+          document.addEventListener('click', eventHandler);
+        });
+      } else {}
     }
   }
 };
@@ -14243,10 +14256,30 @@ exports.default = _default;
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "popover", on: { click: _vm.xxx } },
+    {
+      staticClass: "popover",
+      on: {
+        click: function ($event) {
+          $event.stopPropagation()
+          return _vm.xxx.apply(null, arguments)
+        },
+      },
+    },
     [
       _vm.visible
-        ? _c("div", { staticClass: "content-wrapper" }, [_vm._t("content")], 2)
+        ? _c(
+            "div",
+            {
+              staticClass: "content-wrapper",
+              on: {
+                click: function ($event) {
+                  $event.stopPropagation()
+                },
+              },
+            },
+            [_vm._t("content")],
+            2
+          )
         : _vm._e(),
       _vm._v(" "),
       _vm._t("default"),
