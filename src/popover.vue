8fba1bc979e82da2bb1cd59fcf87073789f86902
Ivyzhao <1490217104@qq.com>
2022-05-17 13:56:20 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible"> 
         <slot name="content" ></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block">
         <slot ></slot>
     </span>
     </div>
@@ -26,6 +26,9 @@
         if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
         ) { return }
+        if(this.$refs.contentWrapper &&
+            this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target)
+        ){return}
         this.close()
         },
         open () {
@@ -54,6 +57,8 @@
 </script>
 
 <style lang="scss" scoped>
+$border-color:#333;
+$border-radius: 4px;
     .popover{
         display: inline-block;
         vertical-align: top;
@@ -61,9 +66,36 @@
     }
     .content-wrapper{
             position: absolute;
-            border:1px solid red;
-            box-shadow:0 0 3px rgba( 0,0, 0,0.5);
+            border:1px solid $border-color;
+            border-radius: $border-radius;
+            filter: drop-shadow(0  2px rgba( 0,0, 0,0.5)) ;
+            background-color: white;
             transform: translateY(-100%);
-             
+            margin-top:-10px;
+            padding:.5em 1em;
+            max-width: 20em;
+            word-break: break-all;
+            &::before{
+                content: '';
+                display: block;
+                border: 10px solid transparent;
+                border-top-color: $border-color;
+                width: 0;
+                height: 0;
+                position: absolute;
+                top:  calc(100% + 1px);
+                left: 10px;
+            }
+            &::after{
+                content: '';
+                display: block;
+                border: 10px solid transparent;
+                border-top-color: white;
+                width: 0;
+                height: 0;
+                position: absolute;
+                top: 100%;
+                left: 10px;
+            }
     }
 </style>
