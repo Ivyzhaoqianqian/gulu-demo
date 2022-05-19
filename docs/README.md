6c06b853b37ffc92bb2e2b9b123a82abaaaac8e2
Ivyzhao <1490217104@qq.com>
2022-05-19 15:56:04 +0800

    初步建立官网

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# GuLu UI 官网
+
+GuLu UI 是一个好用的UI框架
--- src/button.vue
+++ src/button.vue
@@ -38,53 +38,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "var";
 @keyframes spin {
     0% {transform: rotate(0deg);}
     100% {transform: rotate(360deg);}
 }
 
-.g-button {
-            font-size: var(--font-size);
-            height: var(--button-height);
-            padding: 0 1em;
-            border-radius: var(--border-radius);
-            border: 1px solid var(--border-color);
-            background: var(--button-bg);
-            display: inline-flex;
-            justify-content: center;
-            align-items: center;
-            vertical-align: middle;
-            &:hover {
-                border-color: var(--border-color-hover);
-            }
-
-            &:active {
-                background-color: var(--button-active-bg);
-            }
-
-            &:focus {
-                outline: none;
-            }
-            > .icon{
-                order: 1;
-                margin-left: 0;
-                margin-right: .3em;
-            }
-            > .content{
-                order: 2;
-            }
-            &.icon-right{
-                > .icon{
-                    order:2;
-                    margin-right: 0;
-                    margin-left: .3em;
-                }
-                > .content{
-                    order: 1
-                }
-            }
-            .loading{
-                animation: spin 2s infinite linear;
-            }
-};            
+.g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+    border-radius: $border-radius; border: 1px solid $border-color;
+    background: $button-bg;
+    display: inline-flex; justify-content: center; align-items: center;
+    vertical-align: middle;
+    &:hover { border-color: $border-color-hover; }
+    &:active { background-color: $button-active-bg; }
+    &:focus { outline: none; }
+    > .g-button-content { order: 2; }
+    > .icon { order: 1; margin-right: .1em; }
+    &.icon-right {
+      > .g-button-content { order: 1; }
+      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+    }
+    .loading {
+      animation: spin 2s infinite linear;
+    }
+  }
 </style>
